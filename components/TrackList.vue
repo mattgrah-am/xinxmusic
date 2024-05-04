662b6c92907897f41eb31dfabc983e0338ebe570
Matt Graham <iam@mattgrah.am>
2024-05-05 00:51:02 +1000

    update and features

---
+++ components/TrackList.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul>
+    <li class="mb-4">
+      <h3 v-if="title" class="font-bold mb-2 mt-8">
+        {{ title }}
+      </h3>
+      <button
+        @click="$emit('selectTrack', { title: track.title, url: track.url })"
+        v-for="track in tracks"
+        class="py-2 flex gap-4 items-center text-left w-full hover:bg-neutral-200"
+      >
+        <IconPause class="size-4" v-if="track.title === currentTrackTitle" />
+        <IconPlay class="size-4" v-else />
+        {{ track.title }}
+      </button>
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  title?: string;
+  tracks: {
+    title: string;
+    url: string;
+  }[];
+  currentTrackTitle: string;
+}>();
+
+const emits = defineEmits<{
+  selectTrack: (track: { title: string; url: string }) => void;
+}>();
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/fonts", "@nuxtjs/tailwindcss"]
-})
+  devServer: {
+    https: {
+      key: "localhost-key.pem",
+      cert: "localhost.pem",
+    },
+  },
+  modules: ["@nuxt/fonts", "@nuxtjs/tailwindcss"],
+  fonts: {
+    defaults: {
+      weights: [300, 400, 500, 600, 700],
+    },
+  },
+});
